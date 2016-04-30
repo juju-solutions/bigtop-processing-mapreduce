d20d2dcd21be81f2c1a011960858c6564aef3dce
Kevin W Monroe <kevin.monroe@canonical.com>
2016-04-30 11:47:26 -0500

    sentry on client readiness; use smoke-test actions to test bundle

--- tests/01-bundle.py
+++ tests/01-bundle.py
@@ -20,10 +20,11 @@ class TestBundle(unittest.TestCase):
         bundle = yaml.safe_load(bun)
         cls.d.load(bundle)
         cls.d.setup(timeout=1800)
-        cls.d.sentry.wait_for_messages({'slave': 'ready'}, timeout=1800)
+        cls.d.sentry.wait_for_messages({'client': 'Ready'}, timeout=1800)
         cls.hdfs = cls.d.sentry['namenode'][0]
         cls.yarn = cls.d.sentry['resourcemanager'][0]
         cls.slave = cls.d.sentry['slave'][0]
+        cls.client = cls.d.sentry['client'][0]
 
     def test_components(self):
         """
@@ -54,51 +55,26 @@ class TestBundle(unittest.TestCase):
         assert 'DataNode' not in yarn, "DataNode should not be running on resourcemanager"
         assert 'DataNode' not in hdfs, "DataNode should not be running on namenode"
 
-    def test_hdfs_dir(self):
-        """
-        Validate admin few hadoop activities on HDFS cluster.
-            1) This test validates mkdir on hdfs cluster
-            2) This test validates change hdfs dir owner on the cluster
-            3) This test validates setting hdfs directory access permission on the cluster
+    def test_hdfs(self):
+        """Smoke test validates mkdir, ls, chmod, and rm on the hdfs cluster."""
+        unit_name = self.hdfs.info['unit_name']
+        uuid = self.d.action_do(unit_name, 'smoke-test')
+        result = self.d.action_fetch(uuid)
+        # hdfs smoke-test sets outcome=success on success
+        if (result['outcome'] != "success"):
+            error = "HDFS smoke-test failed"
+            amulet.raise_status(amulet.FAIL, msg=error)
 
-        NB: These are order-dependent, so must be done as part of a single test case.
-        """
-        output, retcode = self.client.run("su hdfs -c 'hdfs dfs -mkdir -p /user/ubuntu'")
-        assert retcode == 0, "Created a user directory on hdfs FAILED:\n{}".format(output)
-        output, retcode = self.client.run("su hdfs -c 'hdfs dfs -chown ubuntu:ubuntu /user/ubuntu'")
-        assert retcode == 0, "Assigning an owner to hdfs directory FAILED:\n{}".format(output)
-        output, retcode = self.client.run("su hdfs -c 'hdfs dfs -chmod -R 755 /user/ubuntu'")
-        assert retcode == 0, "seting directory permission on hdfs FAILED:\n{}".format(output)
-
-    def test_yarn_mapreduce_exe(self):
-        """
-        Validate yarn mapreduce operations:
-            1) validate mapreduce execution - writing to hdfs
-            2) validate successful mapreduce operation after the execution
-            3) validate mapreduce execution - reading and writing to hdfs
-            4) validate successful mapreduce operation after the execution
-            5) validate successful deletion of mapreduce operation result from hdfs
-
-        NB: These are order-dependent, so must be done as part of a single test case.
-        """
-        jar_file = '/usr/lib/hadoop-mapreduce/hadoop-mapreduce-examples-*.jar'
-        tests_jar_file = '/usr/lib/hadoop/hadoop-common-*-tests.jar'
-
-        test_steps = [
-            ('teragen',      "su ubuntu -c 'hadoop jar {} teragen  10000 /user/ubuntu/teragenout'".format(jar_file)),
-            ('mapreduce #1', "su hdfs -c 'hdfs dfs -ls /user/ubuntu/teragenout/_SUCCESS'"),
-            ('terasort',     "su ubuntu -c 'hadoop jar {} terasort /user/ubuntu/teragenout /user/ubuntu/terasortout'".
-                format(jar_file)),
-            ('mapreduce #2', "su hdfs -c 'hdfs dfs -ls /user/ubuntu/terasortout/_SUCCESS'"),
-            ('test lzo', "su ubuntu -c 'hadoop jar {} org.apache.hadoop.io.TestSequenceFile -seed 0 \
-                -count 1000 -compressType RECORD xxx -codec org.apache.hadoop.io.compress.LzoCodec \
-                -check'".format(tests_jar_file)),
-            ('cleanup #1',   "su hdfs -c 'hdfs dfs -rm -r /user/ubuntu/teragenout'"),
-            ('cleanup #2',   "su hdfs -c 'hdfs dfs -rm -r /user/ubuntu/terasortout'"),
-        ]
-        for name, step in test_steps:
-            output, retcode = self.client.run(step)
-            assert retcode == 0, "{} FAILED:\n{}".format(name, output)
+    def test_yarn(self):
+        """Smoke test validates teragen/terasort."""
+        unit_name = self.yarn.info['unit_name']
+        uuid = self.d.action_do(unit_name, 'smoke-test')
+        result = self.d.action_fetch(uuid)
+        # yarn smoke-test only returns results on failure; if result is not
+        # empty, the test has failed and has a 'log' key
+        if result:
+            error = "YARN smoke-test failed: %s" % result['log']
+            amulet.raise_status(amulet.FAIL, msg=error)
 
 
 if __name__ == '__main__':
